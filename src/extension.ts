8ed3859307e77ca2360a2da7ba982914c613c724
John Foerch <jjfoerch@grpm.org>
2026-08-29 14:12:16 -0400

    extension.ts: enforce tabSize on initial activation
    This is a workaround for a corner case with VS Code
    where certain configuration items can be ignored
    if the document was opened before the extension
    was activated.  This seems to happen on opening
    a window for the first time when it already has
    a digistar script open in a tab.

--- src/extension.ts
+++ src/extension.ts
@@ -2,14 +2,28 @@
 import * as vscode from 'vscode';
 
 export function activate (context: vscode.ExtensionContext) {
-    console.log('Your extension is now active!');
+    console.log(`Digistar Script extension activated`);
 
-    // Example: Register a basic hello world command
+
+    // Commands
+    //
     let disposable = vscode.commands.registerCommand('digistar.helloWorld', () => {
         vscode.window.showInformationMessage('Hello Digistar!');
     });
 
     context.subscriptions.push(disposable);
+
+
+    // Configuration defaults can be ignored when a window is initially opened and the extension
+    // has not yet been activated.  This is a workaround to enforce our default tabSize or the
+    // configured editor.tabSize for "[digistar]" in local settings.json.
+    vscode.window.visibleTextEditors.forEach(editor => {
+        if (editor.document.languageId === 'digistar') {
+            const config = vscode.workspace.getConfiguration('editor', { languageId: 'digistar' });
+            const activeTabSize = config.get<number>('tabSize');
+            editor.options.tabSize = activeTabSize;
+        }
+    });
 }
 
 export function deactivate () {
