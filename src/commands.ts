638b87318eba97ac43c417bf980422100503d862
John J. Foerch <jjfoerch@gmail.com>
2026-09-10 11:22:12 -0400

    work on tab and enter key behavior

--- src/commands.ts
+++ src/commands.ts
@@ -11,10 +11,28 @@ export async function command_digistarScriptIndentLine (): Promise<void> {
     const document = editor.document;
     const selection = editor.selection;
     const lineText = document.lineAt(selection.active.line).text;
-    const match = lineText.match(/^\s*([0-9+:.]*)\s*(.*?)\s*$/);
+    const match = lineText.match(/^(\s*?)([0-9+:.]*)(\s*)(.*?)\s*$/);
     if (match) {
-        const ts = match[1];
-        const rest = match[2];
+        const ts = match[2];
+        const rest = match[4];
+        // Cursor Motion:
+        //  - if we're in the "rest" part, then we need to back up the cursor
+        //    by the number of characters removed.
+        //  - if we're after the timestamp, we should leave the cursor at the
+        //    start of the rest.
+        const ts_end = match[1].length + ts.length;
+        const rest_begin = ts_end + match[3].length;
+        let new_char_position = selection.active.character;
+        if (selection.active.character >= rest_begin) {
+            const leading_chars_removed = rest_begin - (ts.length + 1);
+            new_char_position -= leading_chars_removed;
+        } else if (selection.active.character >= ts_end) {
+            new_char_position = ts.length + 1;
+        }
+        if (new_char_position != selection.active.character) {
+            const newPosition = new vscode.Position(selection.active.line, new_char_position);
+            editor.selection = new vscode.Selection(newPosition, newPosition);
+        }
         const replacement = ts + '\t' + rest;
         if (replacement !== lineText) {
             await editor.edit(editBuilder => {
@@ -36,24 +54,15 @@ export async function command_digistarIndentLineAndEnter (): Promise<void> {
     const document = editor.document;
     const selection = editor.selection;
     const lineText = document.lineAt(selection.active.line).text;
-    const re = /\s+$/;
-    const match = re.exec(lineText);
-    if (match) {
-        await editor.edit(editBuilder => {
-            const range = new vscode.Range(
-                selection.active.line,
-                match.index,
-                selection.active.line,
-                lineText.length
-            );
-            editBuilder.delete(range);
-        });
-    }
+    const match = lineText.match(/^\s*[0-9+:.]*\s*/);
     let insertText: string;
-    if (indentNewLine == "always") {
-        insertText = '\n\t';
-    } else if (indentNewLine == "never") {
+    // if we are looking at a tab or a timestamp, do not insert a tab
+    if ((match && selection.active.character < match[0].length) ||
+        indentNewLine == "never")
+    {
         insertText = '\n';
+    } else if (indentNewLine == "always") {
+        insertText = '\n\t';
     } else {
         // "auto"
         const prevLineText = document.lineAt(selection.active.line).text;
