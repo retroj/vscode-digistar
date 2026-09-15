389c4d0e73a8f591c01e980063e06c9473f09689
John J. Foerch <jjfoerch@gmail.com>
2026-09-14 20:44:13 -0400

    digistar.indentLine: small parsing correction

--- src/commands.ts
+++ src/commands.ts
@@ -12,7 +12,7 @@ export async function command_digistarScriptIndentLine (): Promise<void> {
     const document = editor.document;
     const selection = editor.selection;
     const lineText = document.lineAt(selection.active.line).text;
-    const match = lineText.match(/^(\s*?)([0-9+:.]*)(\s*)(.*?)\s*$/);
+    const match = lineText.match(/^(\s*)([0-9+:.]*)(\s*)(.*?)\s*$/);
     if (match) {
         const ts = match[2];
         const rest = match[4];
